d4a8d68b8ef58b80335d077451cf1e371bf8a82e
Mohana Mahalakshmi <mohanamahalakshmi1125@gmail.com>
2026-02-27 09:40:44 +0530

    Add files via upload

---
+++ nested_if.py
@@ -0,0 +1,10 @@
+n=int(input())
+if n>=18:
+    print("Adult")
+    if n>=21:
+        print("Run for candidate")
+    else:
+        print("Cannot run for candidate")
+else:
+    print("Not an adult")
+  
