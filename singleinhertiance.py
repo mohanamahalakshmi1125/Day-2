f0b2f37ac76ab363230b370356ff1f05e052c3fb
Mohana Mahalakshmi <mohanamahalakshmi1125@gmail.com>
2026-02-27 09:23:39 +0530

    Delete singleinhertiance.py

--- singleinhertiance.py
+++
@@ -1,9 +0,0 @@
-class Animal: 
-   def speak(self):
-     return "Animal speaks"
-class Dog(Animal):
-   def bark(self):
-     return "Dog barks"
-dog = Dog()
-print(dog.speak()) # Output: Animal speaks
-print(dog.bark()) # Output: Dog barks
